06e9f7ece3979a41c2695b2d9c179a214bb95b9a
Dmitry Balabanov <dymanoid@gmail.com>
2018-05-31 10:49:17 +0200

    Add the initial solution

---
+++ RealTime/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="AssemblyInfo.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RealTime")]
+[assembly: AssemblyDescription("A mod for the Cities:Skylines game")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dymanoid")]
+[assembly: AssemblyProduct("RealTime")]
+[assembly: AssemblyCopyright("Copyright © dymanoid 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
