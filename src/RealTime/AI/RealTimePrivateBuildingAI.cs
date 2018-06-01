2dfd63feab40918ef463c807c930361341da9f90
Dmitry Balabanov <dymanoid@gmail.com>
2018-06-01 18:18:06 +0200

    Implement realistic construction - no construction at night
    - Next step: optionally disable constructions on weekends

---
+++ src/RealTime/AI/RealTimePrivateBuildingAI.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="RealTimePrivateBuildingAI.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace RealTime.AI
+{
+    using Redirection;
+
+    internal sealed class RealTimePrivateBuildingAI
+    {
+        [RedirectFrom(typeof(PrivateBuildingAI))]
+        private static int GetConstructionTime(PrivateBuildingAI instance)
+        {
+            if ((ToolManager.instance.m_properties.m_mode & ItemClass.Availability.AssetEditor) != 0)
+            {
+                return 0;
+            }
+
+            // This causes the constuction to not advance in the night time
+            return SimulationManager.instance.m_isNightTime ? 10880 : 1088;
+        }
+    }
+}
