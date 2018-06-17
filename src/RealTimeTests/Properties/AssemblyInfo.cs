d836802cbbb1684e73093be65feb832d0e618c43
Dmitry Balabanov <dymanoid@gmail.com>
2018-06-17 21:29:04 +0200

    Add unit tests project (empty for now)

---
+++ src/RealTimeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+// <copyright file="AssemblyInfo.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RealTimeTests")]
+[assembly: AssemblyDescription("Unit tests for the Real Time mods")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dymanoid")]
+[assembly: AssemblyProduct("RealTime")]
+[assembly: AssemblyCopyright("Copyright © 2018, dymanoid")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
