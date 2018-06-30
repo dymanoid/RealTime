e816b18fb696c567ae98aebc82ef9f32f0dea98c
dymanoid <9433345+dymanoid@users.noreply.github.com>
2018-06-30 23:07:55 +0200

    Fix issue causing teens and children to unexpectedly return to school

--- src/RealTime/CustomAI/RealTimeResidentAI.Common.cs
+++ src/RealTime/CustomAI/RealTimeResidentAI.Common.cs
@@ -183,7 +183,7 @@ namespace RealTime.CustomAI
                     {
                         case ItemClass.Service.Commercial:
                             if (CitizenProxy.GetWorkBuilding(ref citizen) != 0 && IsWorkDay
-                                && TimeInfo.CurrentHour > Config.LunchBegin && TimeInfo.CurrentHour < Config.WorkEnd)
+                                && TimeInfo.CurrentHour > Config.LunchBegin && TimeInfo.CurrentHour < GetSpareTimeBeginHour(CitizenProxy.GetAge(ref citizen)))
                             {
                                 return ResidentState.AtLunch;
                             }
