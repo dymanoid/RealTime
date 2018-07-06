730168af8bbeac1c9b963b18f961677025384936
dymanoid <9433345+dymanoid@users.noreply.github.com>
2018-07-07 01:51:06 +0200

    Implement #3 - replace direct method redirection with Harmony library

--- src/RealTime/GameConnection/PrivateBuildingAIHook.cs
+++ src/RealTime/GameConnection/PrivateBuildingAIHook.cs
@@ -4,17 +4,23 @@
 
 namespace RealTime.GameConnection
 {
+    using Harmony;
     using RealTime.CustomAI;
-    using Redirection;
 
     internal static class PrivateBuildingAIHook
     {
         public static RealTimePrivateBuildingAI RealTimeAI { get; set; }
 
-        [RedirectFrom(typeof(PrivateBuildingAI))]
-        private static int GetConstructionTime(PrivateBuildingAI instance)
+        [HarmonyPatch(typeof(PrivateBuildingAI), nameof(GetConstructionTime))]
+        private static class GetConstructionTime
         {
-            return RealTimeAI?.GetConstructionTime() ?? 0;
+#pragma warning disable SA1313 // Parameter names must begin with lower-case letter
+            private static bool Prefix(ref int __result)
+#pragma warning restore SA1313 // Parameter names must begin with lower-case letter
+            {
+                __result = RealTimeAI?.GetConstructionTime() ?? 0;
+                return false;
+            }
         }
     }
 }
---
+++ src/RealTime/Patching/MethodPatcher.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="MethodPatcher.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace RealTime.Patching
+{
+    using System.Linq;
+    using System.Reflection;
+    using Harmony;
+
+    /// <summary>
+    /// A class that uses Harmony library for redirecting the game's methods.
+    /// </summary>
+    internal sealed class MethodPatcher
+    {
+        private const string HarmonyId = "com.cities_skylines.dymanoid.realtime";
+
+        private readonly HarmonyInstance harmony;
+
+        /// <summary>Initializes a new instance of the <see cref="MethodPatcher"/> class.</summary>
+        public MethodPatcher()
+        {
+            harmony = HarmonyInstance.Create(HarmonyId);
+        }
+
+        /// <summary>Applies all patches defined in the current assembly.</summary>
+        public void Apply()
+        {
+            Revert();
+            harmony.PatchAll(typeof(MethodPatcher).Assembly);
+        }
+
+        /// <summary>Reverts all patches, if any.</summary>
+        public void Revert()
+        {
+            foreach (MethodBase method in harmony.GetPatchedMethods().ToList())
+            {
+                harmony.RemovePatch(method, HarmonyPatchType.All, HarmonyId);
+            }
+        }
+    }
+}
