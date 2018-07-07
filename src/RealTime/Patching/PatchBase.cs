8ccc3c0bdd54970290a88df15f23ea881ef78f5a
dymanoid <9433345+dymanoid@users.noreply.github.com>
2018-07-08 00:21:41 +0200

    Implement the Harmony patches abstraction classes

---
+++ src/RealTime/Patching/PatchBase.cs
@@ -0,0 +1,82 @@
+﻿// <copyright file="PatchBase.cs" company="dymanoid">Copyright (c) dymanoid. All rights reserved.</copyright>
+
+namespace RealTime.Patching
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// A base class for all method patch classes. The derived classes must define at least one static method with names
+    /// 'Prefix' and/or 'Postfix' and provide the method to patch by overriding the <see cref="GetMethod"/> abstract method.
+    /// </summary>
+    /// <seealso cref="IPatch"/>
+    internal abstract class PatchBase : IPatch
+    {
+        private MethodInfo method;
+
+        /// <summary>Applies the method patch using the specified <paramref name="patcher"/>.</summary>
+        /// <param name="patcher">The patcher object that can perform the patching.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the argument is null.</exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when no method for patching can be determined, or both the 'Prefix' and 'Postfix' methods are missing
+        /// in the derived type.
+        /// </exception>
+        void IPatch.ApplyPatch(IPatcher patcher)
+        {
+            if (patcher == null)
+            {
+                throw new ArgumentNullException(nameof(patcher));
+            }
+
+            if (method == null)
+            {
+                method = GetMethod();
+            }
+            else
+            {
+                return;
+            }
+
+            if (method == null)
+            {
+                throw new InvalidOperationException("No method specified for patching");
+            }
+
+            MethodInfo prefix = GetType().GetMethod("Prefix", BindingFlags.Static | BindingFlags.NonPublic);
+            MethodInfo postfix = GetType().GetMethod("Postfix", BindingFlags.Static | BindingFlags.NonPublic);
+
+            if (prefix == null && postfix == null)
+            {
+                throw new InvalidOperationException("At least one of the 'Prefix' and 'Postfix' methods must be defined");
+            }
+
+            patcher.ApplyPatch(method, prefix, postfix);
+        }
+
+        /// <summary>
+        /// Reverts the method patch using the specified <paramref name="patcher"/> Has no effect if no patches have been
+        /// applied previously.
+        /// </summary>
+        /// <param name="patcher">The patcher object that can perform the patching.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the argument is null.</exception>
+        void IPatch.RevertPatch(IPatcher patcher)
+        {
+            if (patcher == null)
+            {
+                throw new ArgumentNullException(nameof(patcher));
+            }
+
+            if (method != null)
+            {
+                patcher.RevertPatch(method);
+                method = null;
+            }
+        }
+
+        /// <summary>
+        /// When overridden in derived classes, gets the <see cref="MethodInfo"/> instance of the method to patch.
+        /// </summary>
+        /// <returns>A <see cref="MethodInfo"/> instance of the method to patch.</returns>
+        protected abstract MethodInfo GetMethod();
+    }
+}
---
+++ src/RealTime/Patching/Patcher.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="Patcher.cs" company="dymanoid">Copyright (c) dymanoid. All rights reserved.</copyright>
+
+namespace RealTime.Patching
+{
+    using System;
+    using System.Reflection;
+    using Harmony;
+
+    /// <summary>An <see cref="IPatcher"/> implementation based on the Harmony library.</summary>
+    /// <seealso cref="IPatcher"/>
+    internal sealed class Patcher : IPatcher
+    {
+        private readonly HarmonyInstance harmony;
+
+        /// <summary>Initializes a new instance of the <see cref="Patcher"/> class.</summary>
+        /// <param name="harmony">The harmony instance to use for patching.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="harmony"/> is null.</exception>
+        public Patcher(HarmonyInstance harmony)
+        {
+            this.harmony = harmony ?? throw new ArgumentNullException(nameof(harmony));
+        }
+
+        /// <summary>
+        /// Applies a patch to the specified <paramref name="method"/>. At least one called method must be specified.
+        /// </summary>
+        /// <param name="method">The method to patch.</param>
+        /// <param name="prefixCall">The prefix method to call before the <paramref name="method"/>. Can be null.</param>
+        /// <param name="postfixCall">The postfix method to call after the <paramref name="method"/>. Can be null.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="method"/> is null.</exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when both <paramref name="prefixCall"/> and <paramref name="postfixCall"/> are null.
+        /// </exception>
+        public void ApplyPatch(MethodInfo method, MethodInfo prefixCall, MethodInfo postfixCall)
+        {
+            if (method == null)
+            {
+                throw new ArgumentNullException(nameof(method));
+            }
+
+            if (prefixCall == null && postfixCall == null)
+            {
+                throw new ArgumentException($"Both {nameof(prefixCall)} and {nameof(postfixCall)} cannot be null at the same time.");
+            }
+
+            harmony.Patch(method, new HarmonyMethod(prefixCall), new HarmonyMethod(postfixCall));
+        }
+
+        /// <summary>Reverts a patch from the specified <paramref name="method"/>.</summary>
+        /// <param name="method">The method to patch.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="method"/> is null.</exception>
+        public void RevertPatch(MethodInfo method)
+        {
+            if (method == null)
+            {
+                throw new ArgumentNullException(nameof(method));
+            }
+
+            harmony.RemovePatch(method, HarmonyPatchType.All, harmony.Id);
+        }
+    }
+}
