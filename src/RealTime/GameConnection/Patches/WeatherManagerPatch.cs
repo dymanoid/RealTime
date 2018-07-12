f93cf13111bbaf06ff10b38f2ac80687c6bb698d
woosuk park <bjh13579@gmail.com>
2018-07-13 06:38:02 +0900

    Merge remote-tracking branch 'upstream/master'
    damm removed .ds_store
    
    damm removed .ds_store
    City Wake/Night Translation
    
    Update ko.xml

---
+++ src/RealTime/GameConnection/Patches/WeatherManagerPatch.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="WeatherManagerPatch.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace RealTime.GameConnection.Patches
+{
+    using System;
+    using System.Reflection;
+    using RealTime.Patching;
+
+    /// <summary>
+    /// A static class that provides the patch objects for the weather AI .
+    /// </summary>
+    internal static class WeatherManagerPatch
+    {
+        /// <summary>Gets the simulation step implementation.</summary>
+        public static IPatch SimulationStepImpl { get; } = new WeatherManager_SimulationStepImpl();
+
+        /// <summary>Gets the patch object for the simulation method.</summary>
+        private sealed class WeatherManager_SimulationStepImpl : PatchBase
+        {
+            protected override MethodInfo GetMethod()
+            {
+                return typeof(WeatherManager).GetMethod(
+                    "SimulationStepImpl",
+                    BindingFlags.Instance | BindingFlags.NonPublic,
+                    null,
+                    new[] { typeof(int) },
+                    new ParameterModifier[0]);
+            }
+
+#pragma warning disable SA1313 // Parameter names must begin with lower-case letter
+            private static void Prefix(ref float ___m_temperatureSpeed, float ___m_targetTemperature, float ___m_currentTemperature)
+            {
+                // The maximum temperature change speed is now 1/20 of the original
+                float delta = Math.Abs((___m_targetTemperature - ___m_currentTemperature) * 0.000_05f);
+                delta = Math.Min(Math.Abs(___m_temperatureSpeed) + 0.000_01f, delta);
+                ___m_temperatureSpeed = delta - 0.000_099f;
+            }
+#pragma warning restore SA1313 // Parameter names must begin with lower-case letter
+        }
+    }
+}
---
+++ src/RealTime/Simulation/AwakeSleepSimulation.cs
@@ -0,0 +1,152 @@
+﻿// <copyright file="AwakeSleepSimulation.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace RealTime.Simulation
+{
+    using System;
+    using System.Reflection;
+    using ColossalFramework.IO;
+    using RealTime.Config;
+    using RealTime.Tools;
+
+    /// <summary>
+    /// A simulation class that decouples the cities' 'night time' from the sun position.
+    /// CAUTION: this class hacks a <see cref="SimulationManager"/> static field using reflection.
+    /// It might cause unexpected game behavior.
+    /// </summary>
+    /// <seealso cref="ISimulationManager" />
+    internal sealed class AwakeSleepSimulation : ISimulationManager
+    {
+        private readonly ThreadProfiler threadProfiler;
+        private readonly RealTimeConfig config;
+
+        private AwakeSleepSimulation(RealTimeConfig config)
+        {
+            threadProfiler = new ThreadProfiler();
+            this.config = config;
+        }
+
+        /// <summary>Installs the simulation into the game using the specified configuration.</summary>
+        /// <param name="config">The configuration to run with.</param>
+        /// <returns><c>true</c> when it succeeds; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the argument is null.</exception>
+        public static bool Install(RealTimeConfig config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            FastList<ISimulationManager> managers = GetSimulationManagers();
+            if (managers == null)
+            {
+                Log.Error("The 'Real Time' mod failed to get the simulation managers");
+                return false;
+            }
+
+            if (managers.m_size == 0)
+            {
+                Log.Error("The 'Real Time' mod failed to register the awake/sleep simulation - no simulation managers found");
+                return false;
+            }
+
+            if (managers.m_buffer[0] is AwakeSleepSimulation)
+            {
+                return true;
+            }
+
+            if (managers.m_buffer[managers.m_buffer.Length - 1] != null)
+            {
+                managers.EnsureCapacity(managers.m_buffer.Length + 4);
+            }
+
+            ++managers.m_size;
+
+            for (int i = managers.m_size - 1; i > 0; --i)
+            {
+                managers.m_buffer[i] = managers.m_buffer[i - 1];
+            }
+
+            managers.m_buffer[0] = new AwakeSleepSimulation(config);
+            return true;
+        }
+
+        /// <summary>Uninstalls the previously installed simulation. Has no effect if no simulation was installed.</summary>
+        public static void Uninstall()
+        {
+            FastList<ISimulationManager> managers = GetSimulationManagers();
+            if (managers == null || managers.m_size == 0 || !(managers.m_buffer[0] is AwakeSleepSimulation))
+            {
+                return;
+            }
+
+            for (int i = 0; i < managers.m_size - 1; ++i)
+            {
+                managers.m_buffer[i] = managers.m_buffer[i + 1];
+            }
+
+            managers.m_buffer[managers.m_size - 1] = null;
+            --managers.m_size;
+        }
+
+        void ISimulationManager.EarlyUpdateData()
+        {
+        }
+
+        void ISimulationManager.GetData(FastList<IDataContainer> data)
+        {
+        }
+
+        string ISimulationManager.GetName()
+        {
+            return "RealTimeAwakeSleepSimulation";
+        }
+
+        ThreadProfiler ISimulationManager.GetSimulationProfiler()
+        {
+            return threadProfiler;
+        }
+
+        void ISimulationManager.LateUpdateData(SimulationManager.UpdateMode mode)
+        {
+        }
+
+        void ISimulationManager.SimulationStep(int subStep)
+        {
+            if (subStep == 0 || !SimulationManager.instance.m_enableDayNight)
+            {
+                return;
+            }
+
+            float currentHour = (float)SimulationManager.instance.m_currentGameTime.TimeOfDay.TotalHours;
+            SimulationManager.instance.m_isNightTime = currentHour < config.WakeupHour || currentHour >= config.GoToSleepUpHour;
+        }
+
+        void ISimulationManager.UpdateData(SimulationManager.UpdateMode mode)
+        {
+        }
+
+        private static FastList<ISimulationManager> GetSimulationManagers()
+        {
+            FieldInfo field;
+            try
+            {
+                field = typeof(SimulationManager).GetField("m_managers", BindingFlags.Static | BindingFlags.NonPublic);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("The 'Real Time' mod failed to get the simulation managers field: " + ex);
+                return null;
+            }
+
+            if (field == null)
+            {
+                Log.Error("The 'Real Time' mod failed to get the simulation managers field");
+                return null;
+            }
+
+            return field.GetValue(SimulationManager.instance) as FastList<ISimulationManager>;
+        }
+    }
+}
